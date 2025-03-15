5e00e237048266800db35fdfc06e7e34a49bd254
minmooo-ya <minhyay01@gmail.com>
2025-03-16 00:17:05 +0900

    [Bronze III] Title: 팩토리얼, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.py
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.py
@@ -1,10 +1,9 @@
-def factorial(N):
-    result = 1
-    for i in range(2, N + 1):
-        result *= i
-    print(result)
-    return result
-
 N = int(input())
-factorial(N)
+result = 1
+for i in range(2, N + 1):
+    result *= i
+print(result)
+
+
+
 
