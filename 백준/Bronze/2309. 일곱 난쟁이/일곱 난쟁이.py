66298aa99a1ef8d1ee3d7dde20754d248a1b4a84
minmooo-ya <minhyay01@gmail.com>
2025-03-17 00:20:55 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,22 +1,13 @@
+from itertools import combinations
 import sys
-#리스트 입력받고
+
 h = [int(sys.stdin.readline().strip()) for _ in range(9)]
-#리스트 전체 합 구함
-total_h=sum(h)
+new_h = []
 
-n1,n2 = 0, 0
+for i in list(combinations(h,7)):
+    if sum(i) == 100:
+        new_h=i
 
-#이중for써서 두명 뽑음
-#a = 전체합 - 100  h[i]+h[j] == a이런식?
-for i in range(9):
-    for j in range(i+1, 9):
-        if total_h - h[i] - h[j] == 100:
-            n1, n2 = h[i], h[j]
-            break
-    if n1 and n2:
-        break
-
-result = [x for x in h if x != n1 and x != n2]
-result.sort()
-for h2 in result:
-    print(h2)
+new_h = list(new_h)
+new_h.sort()
+print("\n".join(map(str, new_h)))
