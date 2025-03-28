6bf9d84a9f6fbca1750a92119632c8695c64dd0c
minmooo-ya <minhyay01@gmail.com>
2025-03-28 15:18:45 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 240 ms, Memory: 57048 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,43 +1,26 @@
 import sys
-sys.setrecursionlimit(200000)  # 깊은 재귀를 위한 제한 증가
 
-# DFS 함수 (반복문 방식으로 변경)
-def dfs(graph, visited, parent, node):
+N = int(sys.stdin.readline())
+
+# 그래프 생성
+graph = [[] for _ in range(N + 1)]
+for _ in range(N - 1):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+# 방문 여부 배열 및 DFS 스택
+visited = [0] * (N + 1)
+
+def dfs(node):
     stack = [node]
-    visited[node] = True
-    
     while stack:
-        current = stack.pop()
-        for neighbor in graph[current]:
-            if not visited[neighbor]:
-                visited[neighbor] = True
-                parent[neighbor] = current
-                stack.append(neighbor)
+        top = stack.pop()
+        for adj in graph[top]:
+            if visited[adj] == 0:
+                visited[adj] = top
+                stack.append(adj)
 
-def find_parents(N, edges):
-    # 그래프 초기화
-    graph = {i: [] for i in range(1, N + 1)}
-    
-    # 간선 정보 입력
-    for u, v in edges:
-        graph[u].append(v)
-        graph[v].append(u)
-    
-    visited = [False] * (N + 1)  # 방문 여부 체크
-    parent = [0] * (N + 1)  # 부모 노드를 저장하는 배열
-    
-    parent[1] = -1  # 루트는 부모가 없으므로 -1로 설정
-    dfs(graph, visited, parent, 1)  # 1번 노드를 루트로 DFS 시작
-    
-    return parent[2:]  # 2번부터 N번 노드까지 부모 노드를 리턴
+dfs(1)
 
-# 입력 받기
-N = int(input())
-edges = [tuple(map(int, input().split())) for _ in range(N - 1)]
-
-# 부모 노드 찾기
-parents = find_parents(N, edges)
-
-# 출력
-for p in parents:
-    print(p)
+print("\n".join(map(str, visited[2:])))
