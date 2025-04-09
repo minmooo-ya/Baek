cbb5bfb6c13dae4c02898bdecdc7274c15b110f2
minmooo-ya <minhyay01@gmail.com>
2025-04-09 23:52:55 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,14 @@
+t = int(input())
+dp = [0] * 12  # n은 최대 11까지
+
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+# dp 배열 채우기
+for i in range(4, 12):
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
+for _ in range(t):
+    n = int(input())
+    print(dp[n])
