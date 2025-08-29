bdfd084d7583d7e8d2c2ab51d0f689c6f35aa6dc
minmooo-ya <minhyay01@gmail.com>
2025-08-29 11:54:27 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 692 ms, Memory: 67044 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,44 +1,28 @@
+#bfs
 import sys
 from collections import deque
+inout = sys.stdin.readline
 
-# BFS 탐색 함수
-def bfs(graph, check, start):
-    queue = deque([start])
-    check[start] = True
-    
-    while queue:
-        node = queue.popleft()
-        
-        for next_node in graph[node]:
-            if not check[next_node]:
-                check[next_node] = True
-                queue.append(next_node)
+N, M = map(int, inout().split())
+graph = [[] for _ in range(N+1)]
 
-# 연결 요소 개수 구하는 함수
-def get_connected_components(N, graph):
-    check = [False] * (N + 1)  # 방문 여부 체크
-    ans = 0
-    
-    for node in range(1, N + 1):
-        if not check[node]:  # 방문하지 않았다면
-            bfs(graph, check, node)  # BFS 탐색
-            ans += 1  # 새로운 연결 요소 발견
-
-    return ans
-
-# 입력 받기 (sys.stdin.read()로 최적화)
-input = sys.stdin.read
-data = input().splitlines()
-N, M = map(int, data[0].split())
-
-# 그래프 초기화
-graph = {i: [] for i in range(1, N + 1)}  # 1-based 인덱스를 위한 그래프 생성
-
-# 간선 정보 입력 처리
-for i in range(1, M + 1):
-    u, v = map(int, data[i].split())
+for _ in range(M):
+    u, v = map(int, inout().split())
     graph[u].append(v)
-    graph[v].append(u)  # 방향 없는 그래프이므로 양방향으로 간선 추가
+    graph[v].append(u)
+visited = [0] * (N+1)
+count = 0
 
-# 연결 요소 개수 출력
-print(get_connected_components(N, graph))
+for i in range(1, N+1):
+    if not visited[i]:
+        q=deque()
+        visited[i] = 1
+        q.append(i)
+        while q:
+            cur = q.popleft()
+            for j in graph[cur]:
+                if not visited[j]:
+                    visited[j] = 1
+                    q.append(j)
+        count += 1
+print(count)
